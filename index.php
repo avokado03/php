a8811656418dce4dddc436fe5769de4457798f17
Avokado <alotofcode95@gmail.com>
2018-06-10 15:57:07 +0600

    12333

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
-<?
-/**
- * Created by PhpStorm.
- * User: hz
- * Date: 10.06.2018
- * Time: 12:44
- */
-echo 'hello world';
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>я пыталась</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster&amp;subset=cyrillic-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <p>IMAGE</p>
+    </header>
+    <nav>
+        <div>
+            <div class="l">L</div>
+            <div class="logo">ogo</div> 
+        </div>
+    </nav>
+    <main>
+        <div id="nav">
+
+        </div>
+        <div id="mainInfo">
+
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
